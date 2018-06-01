009c88e074644220ca92d9872472b277fe8601ba
Michael Easter <codetojoy@gmail.com>
2018-05-31 21:49:32 -0300

    incremental change as part of a larger frenzy of activity

---
+++ egg_34_stack_overflow_50630886/DSL_v2.groovy
@@ -0,0 +1,43 @@
+
+class Copy { 
+    def copy = { a, b -> println "TRACER copy ${a} to ${b}" }
+    def copyGroovy = { p1, p2 -> copy(p1,p2) }
+    def copyFixP1 = { p1 -> copyGroovy.curry(p1) }
+}
+
+class Copier {
+    def copyContext 
+    def runContext
+    def p1, p2
+
+    def Copier(def copyContext, def runContext) {
+        this.copyContext = copyContext
+        this.runContext = runContext
+    }
+
+    def methodMissing(String name, args) {
+        p1 = args[0]
+        return this
+    }
+
+    Object getProperty( String property ) {
+        p2 = runContext.getProperty(property)
+        copyContext.copyFixP1(p1)(p2)
+    }
+}
+
+// ----- main 
+
+class Example {
+    def src = "A"
+    def dest = "B"
+
+    def run = { ->
+        def cp = new Copier(new Copy(), this)
+
+        cp src dest
+    }
+}
+
+new Example().run()
+
