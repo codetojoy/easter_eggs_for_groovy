ed6b914d0ba088962f4efbacdd207ecebb554f22
Michael Easter <codetojoy@gmail.com>
2018-08-01 11:05:34 -0300

    use regex

--- egg_36_stack_overflow_51620520/Example.groovy
+++ egg_36_stack_overflow_51620520/Example.groovy
@@ -7,25 +7,28 @@ import org.joda.time.*
 def getDateTime(def s) {
     def result = null
 
-    // yyyy-MM-dd, strict
-    def formatter = new DateTimeFormatterBuilder()
-                         .appendFixedDecimal(DateTimeFieldType.year(),4)
-                         .appendLiteral('-')
-                         .appendFixedDecimal(DateTimeFieldType.monthOfYear(),2)
-                         .appendLiteral('-')
-                         .appendFixedDecimal(DateTimeFieldType.dayOfMonth(),2)
-                         .toFormatter()
-    try {
-        result = formatter.parseDateTime(s)
-    } catch (Exception ex) {
-        // System.err.println "TRACER ex: " + ex.message
+    def regex = /^\d\d\d\d-\d\d-\d\d$/
+
+    if (s ==~ regex) {
+        // yyyy-MM-dd, strict
+        def formatter = new DateTimeFormatterBuilder()
+                             .appendYear(4,4)
+                             .appendLiteral('-')
+                             .appendMonthOfYear(2)
+                             .appendLiteral('-')
+                             .appendDayOfMonth(2)
+                             .toFormatter()
+        try {
+            result = formatter.parseDateTime(s)
+        } catch (Exception ex) {
+            // System.err.println "TRACER ex: " + ex.message
+        }
     }
 
     return result
 }
 
 // --- main
-
 
 assert new DateTime(2018,8,2,0,0) == getDateTime('2018-08-02')
 assert null == getDateTime('18-08-02')
